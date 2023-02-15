ef411638760a70cdb5d7f4a91c7576aa009b4f0d
esteban-lm <98118577+esteban-lm@users.noreply.github.com>
2023-02-15 13:21:44 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,9 +1,13 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    query_string = f"(iso_a3 == '{country_code.upper()}' and (date > '{year}-01-01' and date <'{year}-12-31'))"
+    price_year_df = df.query(query_string)
+    mean_price_country = round(price_year_df['dollar_price'].mean(),2)
+    return mean_price_country
 
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
@@ -15,4 +19,8 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    input_year = input("ENTER YEAR: ")
+    input_country_code = input("ENTER COUNTRY CODE: ")
+
+    answer = get_big_mac_price_by_year(input_year,input_country_code)
+    print(answer)
