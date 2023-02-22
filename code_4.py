26f8afc07407647b0f0813cbdf71069cf4274a2f
esteban-lm <98118577+esteban-lm@users.noreply.github.com>
2023-02-22 15:07:23 -0700

    last_one

--- code_4.py
+++ code_4.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv(big_mac_file)
@@ -19,16 +18,16 @@ def get_the_cheapest_big_mac_price_by_year(year):
     query_string = (f"date >= '{year}-01-01' and date <= '{year}-12-31'")
     min_df = df.query(query_string)
     min_df_idx = min_df['dollar_price'].idxmin()
-    min_item = min_df.loc[min_df_idx]
-    min_answer = f"{min_item['name']}({min_item['iso_a3']}): ${round(min_item['dollar_price'],2)}"
+    idx_min_item = min_df.loc[min_df_idx]
+    min_answer = f"{idx_min_item['name']}({idx_min_item['iso_a3']}): ${round(idx_min_item['dollar_price'],2)}"
     return min_answer
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     query_string = (f"date >= '{year}-01-01' and date <= '{year}-12-31'")
     max_df = df.query(query_string)
     max_df_idx = max_df['dollar_price'].idxmax()
-    max_item = max_df.loc[max_df_idx]
-    max_answer = f"{max_item['name']}({max_item['iso_a3']}): ${round(max_item['dollar_price'],2)}"
+    idx_max_item = max_df.loc[max_df_idx]
+    max_answer = f"{idx_max_item['name']}({idx_max_item['iso_a3']}): ${round(idx_max_item['dollar_price'],2)}"
     return max_answer
 
 if __name__ == "__main__":
@@ -44,5 +43,4 @@ if __name__ == "__main__":
     min_answer = (get_the_cheapest_big_mac_price_by_year(input_year))
     print(min_answer)
     max_answer = (get_the_most_expensive_big_mac_price_by_year(input_year))
-    print(max_answer) 
-    
+    print(max_answer)
